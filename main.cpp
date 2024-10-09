bf513c498383e7e203a666c686da7c6f22f59c43
Alin Alexandru <alin.alex1000@gmail.com>
2024-10-09 18:22:46 +0300

    changed main to test simple parser fnctionality
    Signed-off-by: Alin Alexandru <alin.alex1000@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,34 +3,22 @@
 #include <sstream>
 #include <vector>
 
+#include "Document.hpp"
+#include "HTMLParser.hpp"
 #include "HTMLParser/HTMLTokenizer.hpp"
 
 int main(int argc, char const *argv[])
 {
     HTMLTokenizer tokenizer;
-    std::ifstream file("test.html");
-    if (!file.is_open()) {
-        std::cerr << "Failed to open the file." << std::endl;
-        return 1;
-    }
+    std::ifstream file("../test.html");
     std::stringstream bufferStream;
     bufferStream << file.rdbuf();
     std::string buffer = bufferStream.str();
     std::cout << buffer << std::endl;
     std::vector<HTMLToken*> tokens = tokenizer.tokenizeString(buffer);
     std::cout << tokens.size() << " tokens\n";
-    for (auto token : tokens)
-    {
-        std::cout<<"a";
-        if (token->getType() == HTMLToken::TokenType::StartTag || token->getType() == HTMLToken::TokenType::EndTag)
-        {
-            std::cout << token->getName() << ' ' << token->getType() << '\n';
-        }
-        else
-            std::cout << token->getType() << '\n';
-    }
-
-
-
+    HTMLParser parser;
+    Document document = parser.create_document(buffer);
+    document.get_root()->pretty_print();
     return 0;
 }
